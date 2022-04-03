fd27388088df13d110448463caedf0c41a49d21a
Hardcoreyoyo <54779907+Hardcoreyoyo@users.noreply.github.com>
2022-04-03 16:57:59 +0800

    修改字段

--- AllStart.sh
+++ AllStart.sh
@@ -4,7 +4,8 @@ mv -f ./SpringBootElasticsearchCRUD/target/SpringBootElasticsearchCRUD-SE-CRUD-1
 
 docker-compose up -d
 
+sleep 8
 
-# curl -H "Content-Type: application/json" -X POST 'http://localhost:8220/create' -d '{"id":"3","title":"搭建微服務","text":"微服務內容","viewnumber":"1"}'
+curl -H "Content-Type: application/json" -X POST 'http://localhost:8220/create' -d '{"id":"3","title":"搭建微服務","content":"微服務內容","view":"1"}'
 
-# curl -X GET 'http://localhost:8220/read/3'
+curl -X GET 'http://localhost:8220/read/3'
